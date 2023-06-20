b063365d165c72e1df00eddf2061bf0a872685c2
Kem <tuetran25112002@gmail.com>
2023-06-20 19:17:02 +0700

    update port

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,27 @@
-
+import React , {useEffect, useState} from "react"
+import { Note } from "./models/note"
 
 function App() {
- 
+  
+    const [notes, setNotes]=useState<Note[]>([]);
+
+    useEffect(()=>{
+      async function loadNotes(){
+        try{
+          const response= await fetch ("/api/notes",{method: "GET"});
+          const notes = await response.json();
+          setNotes(notes)
+        }catch(error){
+          console.log(error);
+          alert(error);
+        }
+      } 
+      loadNotes();
+    },[])
 
   return (
     <>
-    
+     
     </>
   )
 }
---
+++ front-end/src/api/apiService.ts
@@ -0,0 +1,18 @@
+import { useAxiosWrapper } from "./axiosWrapper";
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const axiosWrapper = useAxiosWrapper();
+
+export const useApi=()=>{
+    return {
+        getNotes,
+    }
+
+    function getNotes(code: string) {
+        const body = {
+          code: code
+        }
+        return axiosWrapper.get(`/visitor`, body)
+      }
+    
+}
